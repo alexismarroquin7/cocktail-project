0090ebc706aa10491260d7c39ca1da70e398e33b
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-03-25 14:50:44 -0700

    centers text

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+
 //components
 import SearchPage from "./components/SearchPage";
+import HomePage from "./components/HomePage";
 
 //style
-import {Grid, makeStyles, Typography, Button} from "@material-ui/core"
+import {Grid, makeStyles} from "@material-ui/core"
 
 const useStyles = makeStyles({
   root: {
     fontFamily: 'roboto, sans-serif'
-  },
-  aTag: {
-    textDecoration: "none",
-    color: "black"
-  },
-  button: {
-    marginBottom: "1rem"
   }
 })
 
 function App() {
 
-  const { push } = useHistory();
   const classes = useStyles();
 
   return (
@@ -34,16 +27,7 @@ function App() {
     
     >
       <Switch>
-        <Route exact path="/">
-          <Typography variant="h3">Search from TheCocktailDB API</Typography>
-          <Grid container
-          direction="column"
-          alignItems="center"
-          >
-          <Button className={classes.button} variant="contained"><a className={classes.aTag} href="https://www.thecocktaildb.com/" target="blank">About the API</a></Button>
-          <Button variant="contained" onClick={() => push("/search")}>Go to search</Button>
-          </Grid>
-        </Route>
+        <Route exact path="/" component={HomePage} />
         <Route path="/search" component={SearchPage}/>
       </Switch>
     </Grid>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//router
 import { BrowserRouter as Router } from "react-router-dom";
 
+//store
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-
 import { reducer } from "./store"
 
+//style
 import App from './App';
 
 const store = createStore(
@@ -27,18 +29,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
