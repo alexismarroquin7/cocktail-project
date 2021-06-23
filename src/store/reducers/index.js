099e7eaad881989fd944b53211036fd60ae02122
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-06-23 10:22:47 -0700

    create separate slice of state for drinks

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,67 +1,8 @@
-import {
-  FETCH_DRINKS_START, 
-  FETCH_DRINKS_SUCCESS, 
-  FETCH_DRINKS_BY_ID_SUCCESS, 
-  FETCH_DRINKS_BY_ID_FAIL,
-  FETCH_DRINKS_FAIL
-} from "../actions";
+import { combineReducers } from "redux";
+import drinksReducer from "./drinksReducer";
 
-const initialState = {
-    drinkName: 'margarita',
-    drinks: [],
-    isLoading: false,
-    error: ''
+const reducers = {
+  drink: drinksReducer
 }
 
-export const reducer = (state = initialState, action) => {
-  switch(action.type){
-    case FETCH_DRINKS_START:
-      return {
-        ...state,
-        isLoading: true
-      }
-    case FETCH_DRINKS_SUCCESS:
-      if(!action.payload.drinksData){
-        return {
-            ...state,
-            drinks: [],
-            isLoading: false,
-            error: `No drinks named '${action.payload.drinkName}' were found.`
-        }
-      }
-      return {
-        ...state,
-        isLoading: false,
-        error: '',
-        drinks: action.payload.drinksData,
-        drinkName: action.payload.drinkName
-      }
-    
-    case FETCH_DRINKS_BY_ID_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        error: '',
-        drinks: action.payload.drinksData,
-        drinkName: state.drinkName
-      }
-    
-    case FETCH_DRINKS_BY_ID_FAIL:
-      return {
-        ...state,
-        drinks: [],
-        isLoading: false,
-        error: action.payload.error
-      }
-      
-    case FETCH_DRINKS_FAIL:
-      return {
-        ...state,
-        isLoading: false,
-        error: action.payload
-      };
-    
-    default:
-      return state;
-  }
-}
+export const reducer = combineReducers(reducers);
